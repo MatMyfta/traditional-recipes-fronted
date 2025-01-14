e90063a3c4a1388f5d11991c789f29e05bf8b239
MatMyfta <mateomyftaraj98@gmail.com>
2025-01-14 18:42:16 +0100

    initial index design with dumb data

---
+++ src/lib/components/recipes/RecipeCard.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let recipe;
+</script>
+
+<!-- <a
+	href={`/recipes/${recipe.id}`}
+	class="block transform transition-transform hover:-translate-y-1 hover:shadow-lg"
+> -->
+<a
+	href="#"
+	class="block transform transition-transform hover:-translate-y-1 hover:shadow-lg"
+>
+	<div class="overflow-hidden rounded-lg bg-white shadow-md h-full">
+        <div class="bg-gray-200 h-48 w-full">
+            {#if recipe.img}
+                <img src={recipe.image} alt={recipe.title} class="h-full w-full object-cover" />
+            {/if}
+        </div>
+		<div class="p-4">
+			<h2 class="mb-2 text-lg font-semibold">{recipe.title}</h2>
+			<p class="text-sm text-gray-600">{recipe.description}</p>
+		</div>
+	</div>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,67 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import RecipeCard from '$lib/components/recipes/RecipeCard.svelte';
+
+	let recipes = [
+		{
+			id: '1',
+			title: 'Spaghetti Carbonara',
+			description: 'A classic Italian pasta dish with egg, cheese, pancetta, and pepper.',
+			image: null
+		},
+		{
+			id: '2',
+			title: 'Tiramisu',
+			description: 'A delicious coffee-flavored dessert.',
+			image: null
+		},
+		{
+			id: '2',
+			title: 'Spaghetti Carbonara',
+			description: 'A classic Italian pasta dish with egg, cheese, pancetta, and pepper.',
+			image: null
+		},
+		{
+			id: '3',
+			title: 'Tiramisu',
+			description: 'A delicious coffee-flavored dessert.',
+			image: null
+		},
+		{
+			id: '4',
+			title: 'Spaghetti Carbonara',
+			description: 'A classic Italian pasta dish with egg, cheese, pancetta, and pepper.',
+			image: null
+		},
+		{
+			id: '5',
+			title: 'Tiramisu',
+			description: 'A delicious coffee-flavored dessert.',
+			image: null
+		},
+	];
+
+    function getAllItems() {
+        return recipes;
+    }
+
+    // TODO: attach to a REST API service
+	async function fetchRecipes() {
+		try {
+			recipes = getAllItems();
+		} catch (err) {
+			console.error('Failed to load recipes:', err);
+		}
+	}
+
+	fetchRecipes();
+</script>
+
+<h1 class="my-8 text-center text-3xl font-bold">Traditional Recipes</h1>
+
+<div class="container mx-auto p-4">
+	<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		{#each recipes as recipe}
+			<RecipeCard {recipe} />
+		{/each}
+	</div>
+</div>
